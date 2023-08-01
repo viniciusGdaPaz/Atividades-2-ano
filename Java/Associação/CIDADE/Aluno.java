57b9e56ff92289a7d80961439262257ed0a84b12
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-01 11:58:36 -0300

    Create Aluno.java

---
+++ Java/Associação/CIDADE/Aluno.java
@@ -0,0 +1,40 @@
+package modelo;
+
+public class Aluno {
+ 
+	
+	private String nome ;
+	private Integer idade; 
+	private Turma turma ;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	public Turma getTurma() {
+		return turma;
+	}
+	public void setTurma(Turma turma) {
+		this.turma = turma;
+	}
+	
+	
+	
+	
+}
