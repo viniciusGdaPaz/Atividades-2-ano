40b65819480394c472cf681864364f263a2185b5
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-01 11:59:13 -0300

    Create cidade.java

---
+++ Java/Associação/CIDADE/cidade.java
@@ -0,0 +1,42 @@
+package modelo;
+
+public class Cidade {
+
+	private String nome;
+	private Integer qtdhabitantes;
+	private Integer altitude;
+	private Estado estado;
+	
+	
+	
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getQtdhabitantes() {
+		return qtdhabitantes;
+	}
+	public void setQtdhabitantes(Integer qtdhabitantes) {
+		this.qtdhabitantes = qtdhabitantes;
+	}
+	public Integer getAltitude() {
+		return altitude;
+	}
+	public void setAltitude(Integer altitude) {
+		this.altitude = altitude;
+	}
+	public Estado getEstado() {
+		return estado;
+	}
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+}
