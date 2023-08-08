84df9d93fc491d3a35e893fdee27865af67d237f
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-08 12:01:23 -0300

    Create departamento.java

---
+++ Java/Associação/FUNCIONARIO/departamento.java
@@ -0,0 +1,38 @@
+package modelo;
+
+public class Departamento {
+	
+	//atributos
+	private String nome;
+	private Integer numeroSala;
+	
+	
+	
+	
+	 //metodos
+	
+	
+	
+	
+	//gets and Setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getNumeroSala() {
+		return numeroSala;
+	}
+	public void setNumeroSala(Integer numeroSala) {
+		this.numeroSala = numeroSala;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
