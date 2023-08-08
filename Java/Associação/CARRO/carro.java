4da862db8a9b38de9502bdd1428456fbd563b770
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-08 12:02:51 -0300

    Create carro.java

---
+++ Java/Associação/CARRO/carro.java
@@ -0,0 +1,43 @@
+package modelo;
+
+public class Carro {
+
+	  //atributos
+	private String modelo;
+	private Integer anoFab;
+	private Fabricante fabricante;
+	
+	
+	
+	
+	
+	
+	//gets and setters
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public Integer getAnoFab() {
+		return anoFab;
+	}
+	public void setAnoFab(Integer anoFab) {
+		this.anoFab = anoFab;
+	}
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
