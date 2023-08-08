92465157cabf91b0b8f272c19ec04d2dfb97fc9f
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-08 12:03:35 -0300

    Create fabricante.java

---
+++ Java/Associação/CARRO/fabricante.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Fabricante {
+
+	
+	//atributos
+		private String nome;
+		private String sigla;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//gets and seters
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public String getSigla() {
+			return sigla;
+		}
+		public void setSigla(String sigla) {
+			this.sigla = sigla;
+		}
+	
+	
+	
+	
+}
