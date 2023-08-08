440cc46533220cc0184acef8905c1e6457c937a3
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-08 12:01:48 -0300

    Create funcionario.java

---
+++ Java/Associação/FUNCIONARIO/funcionario.java
@@ -0,0 +1,49 @@
+package modelo;
+
+public class Funcionario {
+//atributos
+	private String nome;
+	private String cargo;
+	private Float salario;
+	private Departamento depto;
+	
+	
+	//metodos
+	
+	
+	
+	
+	
+	//gets and Setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public Float getSalario() {
+		return salario;
+	}
+	public void setSalario(Float salario) {
+		this.salario = salario;
+	}
+	public Departamento getDepto() {
+		return depto;
+	}
+	public void setDepto(Departamento depto) {
+		this.depto = depto;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
