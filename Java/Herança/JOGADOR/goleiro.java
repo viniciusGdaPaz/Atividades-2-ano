a7c4bc71f0c538af5ae453b8e89c31f12d8a7911
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-22 11:55:46 -0300

    Create goleiro.java

---
+++ Java/Herança/JOGADOR/goleiro.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Goleiro extends Jogador {
+	
+    private String Altura;
+
+    
+    //metodo
+    public String getDefesa() {
+    	String d = "defesaaaaaaa";
+    	return d;
+    }
+    
+    
+    
+    
+    
+    //gets and setters
+    
+	public String getAltura() {
+		return Altura;
+	}
+
+	public void setAltura(String altura) {
+		Altura = altura;
+	}
+ 
+    
+    
+    
+    
+    
+}
