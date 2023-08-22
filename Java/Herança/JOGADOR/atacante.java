4c5acd621849d9f40bee05994d98e3d8fc1db839
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-22 11:56:47 -0300

    Create atacante.java

---
+++ Java/Herança/JOGADOR/atacante.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class Atacante extends Jogador{
+
+	
+	    private Double velocidade;
+
+	    
+	    
+	    //metodos
+	    public String getGoal() {
+	    	String g = "GOAAAAAALLLLLL";
+	    	return g ;
+	    }
+	    
+	    
+	    //gets and setters
+		public Double getVelocidade() {
+			return velocidade;
+		}
+
+		public void setVelocidade(Double velocidade) {
+			this.velocidade = velocidade;
+		}
+	    
+	    
+	    
+	    
+	    
+}
