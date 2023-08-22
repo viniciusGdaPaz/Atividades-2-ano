a3e34b196dfba4e1471056d852d5c4af858917f8
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-08-22 11:56:05 -0300

    Create defensor.java

---
+++ Java/Herança/JOGADOR/defensor.java
@@ -0,0 +1,31 @@
+package modelo;
+
+public class Defensor extends Jogador {
+
+	private Double altura;
+
+	
+	//metodos
+	public String getDesermar() {
+		String d = "DEsarmaaaa tafarel";
+		return d;
+	}
+	
+	public String getCabecear() {
+		String c = "CAbeçaaaaaa";
+		return c;
+	}
+	
+	
+	
+	//gets and settes
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+	
+	
+}
