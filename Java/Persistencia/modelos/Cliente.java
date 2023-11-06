f8dc89933c46262ca3d5e3ef883c8a642c3e4918
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-11-06 09:46:31 -0300

    Create Cliente.java

---
+++ Java/Persistencia/modelos/Cliente.java
@@ -0,0 +1,34 @@
+package modelo;
+
+public abstract class Cliente {
+	
+	protected Integer id;
+	protected String nomeSocial;
+	protected String email;
+	
+	//Métodos
+	public abstract String getNome();
+	public abstract String getNroDoc();
+	public abstract String getTipo();
+	
+	//GETs e SETs
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNomeSocial() {
+		return nomeSocial;
+	}
+	public void setNomeSocial(String nomeSocial) {
+		this.nomeSocial = nomeSocial;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
