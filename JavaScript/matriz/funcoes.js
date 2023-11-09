610338eaf9c90a4341cb57f52c463b9f596332d3
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-11-09 11:47:16 -0300

    Create funcoes.js

---
+++ JavaScript/matriz/funcoes.js
@@ -0,0 +1,28 @@
+var produtos=[[1000,1001,1002],
+              ["Mouse","Teclado","Monitor"],
+              [35,50,350]];
+
+var pos;
+
+              function buscarProduto(){
+    var cod=document.frVendas.cod.value;
+     pos= produtos[0].indexOf(parseInt(cod));
+    document.frVendas.produto.value=produtos[1][pos];
+}
+
+function incluir(){
+    var qtd=parseInt(document.frVendas.qtd.value);
+    var pai = document.getElementById("tabela");
+    tr= document.createElement("tr");
+    for(let i=0; i<3; i++){
+      td=document.createElement("td");
+      td.innerHTML=produtos[i][pos];
+      tr.appendChild(td);
+    }
+    td=document.createElement("td");
+    td.innerHTML=qtd*produtos[2][pos];
+    tr.appendChild(td);
+
+
+    pai.appendChild(tr);
+}
