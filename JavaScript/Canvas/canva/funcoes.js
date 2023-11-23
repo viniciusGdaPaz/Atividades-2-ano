f989318032c23859d1bf2608eb8c2eef0d59fbf3
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2023-11-23 11:38:51 -0300

    Create funcoes.js

---
+++ JavaScript/Canvas/canva/funcoes.js
@@ -0,0 +1,7 @@
+const LARGURA=400, ALTURA=400;
+var pai=document.getElementsByTagName("body")[0];
+var canvas=document.createElement("canvas");
+canvas.setAttribute("width",LARGURA);
+canvas.setAttribute("height",ALTURA);
+canvas.style="background-color: #ABBAEA";
+pai.appendChild(canvas);
